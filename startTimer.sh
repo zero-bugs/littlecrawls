1bafedc551d6a5a02c3d33451229a109cc66932e
zero-bugs <annoymous@example.com>
2020-11-14 11:54:21 +0800

    timer kill...

--- startTimer.sh
+++ startTimer.sh
@@ -3,11 +3,13 @@
 while(true)
 do
   echo "start:===== `date` ====="
-  ps -ef | grep dart | grep -v grep | awk '{print $2}'|xargs -I {} kill -9 {}
+  tasklist //FI "IMAGENAME eq dart.exe"
+  taskkill //F //FI "IMAGENAME eq dart.exe"
   nohup dart ./bin/main.dart >>./main.out.log 2>&1 &
   nohup dart ./bin/test.dart >>./test.out.log 2>&1 &
   echo "end:===== `date` ====="
   
-  sleep 1h
+  #sleep 1h
+  sleep 40m
 done
   
