457293149ed5930d1283c0b5dbb4e998c4060c73
zero-bugs <38752615+zero-bugs@users.noreply.github.com>
2020-12-15 19:32:24 +0800

    Create http_utils.py

---
+++ src/utils/http_utils.py
@@ -0,0 +1,32 @@
+# !/usr/bin/python3
+# -*- coding: UTF-8 -*-
+import time
+
+import requests
+
+from src.common.ProxyConstants import ProxyConstant
+
+
+class HttpClient:
+
+    @classmethod
+    def http_retry_executor(cls, url, headers: dict):
+        retry = False
+        for num in range(0, 5):
+            if retry:
+                print("retry url:{} for {} time".format(url, num))
+            try:
+                if ProxyConstant.proxySwitch:
+                    return requests.get(url, proxies=ProxyConstant.proxies, verify=True, timeout=300, headers=headers)
+                else:
+                    return requests.get(url, verify=True, timeout=300)
+            except Exception as err:
+                print(r"http request failed, retry url:{url}")
+                print(err)
+
+                time.sleep(5)
+
+                retry = True
+                if num == 4:
+                    print("failed at last, please try by hands")
+                    return None
