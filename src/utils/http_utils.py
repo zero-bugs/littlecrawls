9bf7b563f946a9b69a4827e88baf2147abe5beb4
zero-bugs <38752615+zero-bugs@users.noreply.github.com>
2020-12-17 21:24:04 +0800

    Update http_utils.py

--- src/utils/http_utils.py
+++ src/utils/http_utils.py
@@ -1,5 +1,6 @@
 # !/usr/bin/python3
 # -*- coding: UTF-8 -*-
+import json
 import time
 
 import requests
@@ -8,25 +9,24 @@ from src.common.proxy_config import ProxyConstant
 
 
 class HttpClient:
-
-    @classmethod
-    def http_retry_executor(cls, url, headers: dict):
+    @staticmethod
+    def http_retry_executor(url, headers):
         retry = False
         for num in range(0, 5):
             if retry:
                 print("retry url:{} for {} time".format(url, num))
             try:
                 if ProxyConstant.proxySwitch:
-                    return requests.get(url, proxies=ProxyConstant.proxies, verify=True, timeout=300, headers=headers)
+                    return requests.get(url, verify=True, timeout=120, proxies=ProxyConstant.proxies, headers=headers)
                 else:
-                    return requests.get(url, verify=True, timeout=300)
+                    return requests.get(url, verify=True, timeout=120, headers=headers)
             except Exception as err:
-                print(r"http request failed, retry url:{url}")
+                print("http request failed, retry url:{}".format(url))
                 print(err)
 
-                time.sleep(5)
+            time.sleep(5)
 
-                retry = True
-                if num == 4:
-                    print("failed at last, please try by hands")
-                    return None
+            retry = True
+            if num == 4:
+                print("failed at last, please try by hands")
+                return None
